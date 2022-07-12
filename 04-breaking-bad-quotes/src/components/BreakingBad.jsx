dedaeab20abd0aa7d8d546f5f616abe4c6068f79
juancr5 <juancr5@hotmail.com>
2022-07-11 19:36:47 -0500

    Update Aplications

---
+++ 04-breaking-bad-quotes/src/components/BreakingBad.jsx
@@ -0,0 +1,42 @@
+import { useCounter } from "../hooks/useCounter";
+import { useFetch } from "../hooks/useFetch";
+
+const BreakingBad = () => {
+
+  //Variable conatdor
+  const { counter, increment } = useCounter(1);
+  //Desestructuracion del la Data traida del useFetch
+  const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+  //Asignacion de la data si esta tiene informacion condicional con &&
+  const { author, quote } = !!data && data[0];
+
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {
+        isLoading
+          ? <div className="alert alert-info text-center">Loading...</div>
+          :
+          <blockquote className="blockquote text-end">
+            <p className="mb-3" style={{ fontSize: '1.8em' }}>{quote}</p>
+            <footer className="blockquote-footer" style={{ fontSize: '1em' }}>
+              {author}
+            </footer>
+          </blockquote>
+      }
+
+      <button
+        className="btn btn-primary"
+        disabled={isLoading}
+        style={{ fontSize: '1.5em' }}
+        onClick={() => increment()}>
+        Next quote
+      </button>
+
+    </>
+  );
+}
+
+export default BreakingBad;
---
+++ 04-breaking-bad-quotes/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BreakingBad from './components/BreakingBad';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BreakingBad />
+  </React.StrictMode>
+);
