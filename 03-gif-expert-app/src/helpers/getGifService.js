101147e700f87ba9ec84e20aabf75015208844e6
juancr5 <juancr5@hotmail.com>
2022-07-28 20:51:14 -0500

    Update Aplications

--- 03-gif-expert-app/src/helpers/getGifService.js
+++ 03-gif-expert-app/src/helpers/getGifService.js
@@ -1,9 +1,9 @@
 //Consumir la api y desestructurar la informacion
 export const getGifsService = async (category) => {
 
-    const apikey = "E819tRW8kVAi2v89wjsr2bGw1WiS2nBp&q"
+    const apikey = "E819tRW8kVAi2v89wjsr2bGw1WiS2nBp&q";
     //Category le pasara el parametro de la busqueda al query de la url
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}=${ category }&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}=${ category }&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
